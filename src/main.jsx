db57a2662522e926dc91f0926189a107139300f7
Liam Brophy <liambrophy.design@gmail.com>
2025-02-05 17:31:58 -0500

    completes lab

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from './routes.jsx';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={createBrowserRouter(routes)} />
+  </React.StrictMode>
+);
